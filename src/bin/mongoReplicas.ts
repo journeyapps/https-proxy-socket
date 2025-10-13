838836baddab7555a97e19d7b030b7fc80cba7c0
EspressoTrip-v2 <juan@journeyapps.com>
2025-10-13 09:34:09 +0200

    comments

--- src/bin/mongoReplicas.ts
+++ src/bin/mongoReplicas.ts
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 import { resolveSrv } from 'dns/promises';
+
+/**
+ * NPX script to resolve MongoDB SRV records.
+ * @example npx @journeyapps/https-proxy-socket mongo-replicas mongodb+srv://<username>:<password>@cluster1.vlnzcbp.mongodb.net
+ * @example npx @journeyapps/https-proxy-socket mongo-replicas mongodb+srv://cluster1.vlnzcbp.mongodb.net
+ *
+ * @returns An object with a `replicas` property containing the resolved host:port pairs.
+ */
 async function mongoReplicas() {
   const srvUrl = process.argv[3];
   const url = new URL(srvUrl);
